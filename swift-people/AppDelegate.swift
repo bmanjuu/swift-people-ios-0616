bfc2a75fc850ee8e632334cc8ed334547afebbb0
bmanjuu <bettyofung@gmail.com>
2016-07-19 13:15:46 -0400

    complete

--- swift-people/AppDelegate.swift
+++ swift-people/AppDelegate.swift
@@ -10,6 +10,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        let betty: Person
+        betty = Person.init(name: "Betty", ageInYears: 26)
+        print(betty.celebrateBirthday())
+        
+         print("Before learning skills:\n Qualified Tutor? \(betty.qualifiedTutorCheck())")
+        
+        betty.learnSkillXcode()
+        betty.learnSkillInterfaceBuilder()
+        betty.learnSkillObjectiveC()
+        betty.learnSkillSwift()
+        betty.learnSkillXcode()
+        
+        print("Qualified now? \(betty.qualifiedTutorCheck())")
         
         /**
 
--- swift-people/Person.swift
+++ swift-people/Person.swift
@@ -11,27 +11,58 @@ import Foundation
 class Person {
     private let name : String
     private var ageInYears : Int?
-    private var skills : [String]
-    private var qualifiedTutor : Bool {
-        return false
-    }
+    private var skills : [String] = []
+    private var qualifiedTutor : Bool {return self.skills.count >= 4}
+    //whenever you have a conditional (>=, ==, etc), if/else statement is implied, it returns a bool!! 
     
-    init(name: name, ageInYears: ageInYears) {
+    //initializers
+    init(name: String, ageInYears: Int?) {
         self.name = name
-        self.ageInYears = {
-            if let ageInYearsProperty = ageInYears {
-                return ageInYears
-            }
-        }
+        self.ageInYears = ageInYears
     }
     
-    convenience init(name: name){
+    convenience init(name: String){
         self.init(name: name, ageInYears: nil)
     }
     
     convenience init(){
-        self.name = "John Doe"
-        self.ageInYears = nil
+        self.init(name: "John Doe", ageInYears: nil)
+    }
+    
+    //functions
+    func celebrateBirthday() -> String {
+        if var age = self.ageInYears {
+            age += 1
+            return "HAPPY \(age)\(age.ordinal().uppercaseString) BIRTHDAY, \(self.name.uppercaseString)!!!"
+        }
+        else {
+            return "HAPPY BIRTHDAY, \(self.name.uppercaseString)!!!"
+        }
+    }
+    
+    func learnSkillBash() {
+        self.skills.append("bash")
+    }
+    
+    func learnSkillXcode() {
+        self.skills.append("Xcode")
+    }
+    
+    func learnSkillObjectiveC() {
+        self.skills.append("Objective-C")
+    }
+    
+    func learnSkillSwift() {
+        self.skills.append("Swift")
+    }
+    
+    func learnSkillInterfaceBuilder() {
+        self.skills.append("Interface Builder")
+    }
+    
+    func qualifiedTutorCheck() -> Bool {
+        print(self.qualifiedTutor)
+        return self.qualifiedTutor
     }
     
 }
